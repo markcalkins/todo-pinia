fa76b46175e7e2c121751ae6624f88b88c17c8f4
Mark Calkins <mcalkins@gmail.com>
2023-01-28 12:09:48 -0700

    Initial Todo App

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import TodoApp from './components/TodoApp.vue'
+</script>
+
+<template>
+  <todo-app />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  background-color: #597d9a;
+  display: flex;
+  justify-content: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+input {
+  margin: 0 15px 15px 50px;
+  padding: 5px 10px;
+}
+button {
+  padding: 7px 14px;
+  background-color: #008CFF;
+  border: none;
+  color: white;
+}
+</style>
